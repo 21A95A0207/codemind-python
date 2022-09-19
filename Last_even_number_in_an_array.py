a2a8c76a53f4d0f26820da51bb9bb15b7b08a63e
21A95A0207 <102217104+21A95A0207@users.noreply.github.com>
2022-09-19 05:32:30 +0530

    n=int(input()) elements=list(map(int,input().split())) count=0 for i in range(0,n):     if elements[i]%2==0:         ev=elements[i]     else:         count+=0 print(ev)

---
+++ Last_even_number_in_an_array.py
@@ -0,0 +1,9 @@
+n=int(input())
+elements=list(map(int,input().split()))
+count=0
+for i in range(0,n):
+    if elements[i]%2==0:
+        ev=elements[i]
+    else:
+        count+=0
+print(ev)        
